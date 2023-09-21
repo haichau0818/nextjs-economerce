1310a517b4d39aff5aa3e261e095c1f29f8d5634
kane chau <haichau0818@gmail.com>
2023-09-21 15:42:27 +0700

    login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,11 +3,42 @@
 import { loginFormControls } from "@/utils";
 import InputComponent from "@/components/FormElements/InputComponent";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { login } from "@/services/login";
 
+const innitalFormData={
+    email:'',
+    password:'',
+
+}
 
 export default function Login(){
-    
-    const router= useRouter()
+
+    const [formData,setFormData]= useState(innitalFormData);
+
+    const router= useRouter();
+
+    console.log(formData);
+
+
+    function isFormValidate(){
+
+      return formData &&
+        formData.email &&
+        formData.email.trim() !== "" &&
+        formData.password &&
+        formData.password.trim() !== ""
+        ? true
+        : false; 
+    }
+
+    async function handleLogin(){
+
+      const req= await login(formData);
+
+      console.log(req);       
+    }
+
     return (
       <div className="bg-white relative text-black">
         <div className="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-0 mt-8 mr-auto xl:px-5 lg:flex-row">
@@ -25,10 +56,12 @@ export default function Login(){
                         type={controlItem.type}
                         placeholder={controlItem.placeholder}
                         label={controlItem.label}
+                        value={formData[controlItem.id]}
+                        onChange={(event)=>{setFormData({...formData,[controlItem.id]:event.target.value})}}
                       />
                     ) : null
                   )}
-                  <button className="inline-flex w-full items-center justify-center bg-fuchsia-700 px-6 py-4 text-lg text-white transition-all duration-200 ease-in-out focus:shadow font-medium upercase tracking-wide rounded-xl">
+                  <button className="disable:opacity-50 inline-flex w-full items-center justify-center bg-fuchsia-700 px-6 py-4 text-lg text-white transition-all duration-200 ease-in-out focus:shadow font-medium upercase tracking-wide rounded-xl" disabled={!isFormValidate()} onClick={handleLogin}>
                     Login
                   </button>
                   <div className="flex flex-col gap-2">
---
+++ src/services/login/index.js
@@ -0,0 +1,21 @@
+
+
+export const login= async(formData)=> {
+
+    try{
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = response.json();
+
+      return data;
+    }catch(e){
+        console.log("error", e);
+    }
+
+}
